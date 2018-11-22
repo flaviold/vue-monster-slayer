c51befb4ce6c13a025f0d1fe3ff161add60f208b
Flavio Duarte <flduarte13@gmail.com>
2018-11-22 13:36:31 -0200

    Game over conditions and player actions

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ new Vue({
   data: {
     gameStarted: false,
     maxHealth: 100,
-    playerLife: 40,
-    monsterLife: 1,
+    playerLife: 100,
+    monsterLife: 100,
     battleLog: []
   },
   computed: {
@@ -30,6 +30,24 @@ new Vue({
 
       this.gameStarted = true
     },
+    checkGameOver() {
+      if (this.playerLife <= 0) {
+        this.gameOver("Monster")
+        return true
+      }
+      if (this.monsterLife <= 0) {
+        this.gameOver("Player")
+        return true
+      }
+    },
+    gameOver(winner) {
+      if (confirm(winner + " wins! Do you want to start a new game?")) {
+        this.newGameStart()
+        return
+      }
+
+      this.gameStarted = false
+    },
     attack() {
       let damage = Math.floor(Math.random() * 7) + 2
 
@@ -39,7 +57,38 @@ new Vue({
         textColor: 'darkblue',
         message: 'Player hits monster for ' + damage
       })
-      this.monsterAttack()
+      if (!this.checkGameOver()) {
+        this.monsterAttack()
+      }
+    },
+    specialAttack() {
+      let damage = Math.floor(Math.random() * 14) + 4
+
+      this.monsterLife -= damage
+      this.battleLog.push({
+        bgColor: 'lightblue',
+        textColor: 'darkblue',
+        message: 'Player special attack the monster for ' + damage
+      })
+      if (!this.checkGameOver()) {
+        this.monsterAttack()
+      }
+    },
+    heal() {
+      let healing = Math.floor(Math.random() * 14) + 4
+
+      this.playerLife += healing
+
+      this.playerLife = this.playerLife < this.maxHealth ? this.playerLife : this.maxHealth
+
+      this.battleLog.push({
+        bgColor: 'lightgreen',
+        textColor: 'darkgreen',
+        message: 'Player heals for ' + healing
+      })
+      if (!this.checkGameOver()) {
+        this.monsterAttack()
+      }
     },
     monsterAttack() {
       let damage = Math.floor(Math.random() * 7) + 2
@@ -50,6 +99,7 @@ new Vue({
         textColor: 'darkred',
         message: 'Monster hits player for ' + damage
       })
+      this.checkGameOver()
     }
   }
 })
